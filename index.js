fa88be51c2bf4b0a8c333b6a45dcfc393684360e
Bruno R. Holanda <rf1792@ucdb.br>
2023-05-19 22:28:17 -0400

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api/v1/list', (request, response)=> {
-    response.send('Hello World');
+    response.send('Hello World1');
 });
 
 app.listen(3000, ()=> console.log('Aplicação iniciada na porta 3000'));
